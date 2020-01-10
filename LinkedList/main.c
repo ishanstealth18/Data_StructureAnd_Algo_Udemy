9bd61c93117289c94fc7815dcdc9e59aa1701b87
Ishan <ishank@lightwavetechnology.com>
2020-01-10 16:39:07 -0500

    Added functions for inserting and deleting new nodes at various position in linked list

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -18,7 +18,7 @@ void traverseList(struct Node* n)
 {
 	while(n  != NULL)
 	{
-		printf("\n%d",n->info);
+		printf("%d ",n->info);
 		n = n->link;
 	}
 }
@@ -97,6 +97,108 @@ void searchNumMethod(int num, struct Node *startNode)
 	}
 }
 
+/**
+Description: This function add a new node at beginning of the linked list.
+Param: NA
+Return: NA
+*/
+void insertAtBeginning()
+{
+	/*Declare variables for new node to be inserted*/
+	int data1 = 0;
+	struct Node* temp1;
+	printf("\nEnter value for node at start: ");
+	scanf("%d",&data1);
+	/*Allocating space for new node*/
+	temp1 = (struct Node *) malloc( sizeof(struct Node) );
+	/*Storing data in the new node*/
+	temp1->info = data1;
+	/*Linking the new node with the start node as address of second node is stored in start node*/
+	temp1->link = start;
+	printf("Updated list: ");
+	traverseList(temp1);
+	insertAtEnd(temp1);
+	traverseList(temp1);
+	insertNodeBetween(temp1);
+	traverseList(temp1);
+	deleteNodeBetween(temp1);
+	traverseList(temp1);
+}
+
+/**
+Description: This function add a new node at end of the linked list.
+Param: variable of type Node
+Return: NA
+*/
+void insertAtEnd(struct Node* n2)
+{
+	/*Declare variables for new node to be inserted*/
+	int data2 = 0;
+	struct Node* temp2;
+	printf("\nEnter value for last node: ");
+	scanf("%d",&data2);
+	/*Allocating space for new node*/
+	temp2 = (struct Node *) malloc( sizeof(struct Node) );
+	temp2->info = data2;
+	/*Logic to fins the end of the linked list where new node can be inserted*/
+    while(n2->link != NULL)
+    {
+        n2 = n2->link;
+    }
+    /*Linking second last node to last node*/
+    n2->link = temp2;
+    /*Linking the last node to NULL value as it do not contain any links further*/
+    temp2->link = NULL;
+}
+
+/**
+Description: This function add a new node between 2 nodes in linked list.
+Param: variable of type Node
+Return: NA
+*/
+void insertNodeBetween(struct Node* n3)
+{
+	/*Declare variables for new node to be inserted*/
+	int data3 = 0;
+	int insertNum = 0;
+	struct Node* temp3;
+	printf("\nAfter what value number is to be inserted? ");
+	scanf("%d",&insertNum);
+	printf("\nEnter value for node in between: ");
+	scanf("%d",&data3);
+	/*Allocating space for new node*/
+	temp3 = (struct Node *) malloc( sizeof(struct Node) );
+	temp3->info = data3;
+	/*Logic to find node after which node is to be inserted*/
+	while(n3->info != insertNum)
+	{
+		n3 = n3->link;
+	}
+	/*Linking the new node to the next node*/
+	temp3->link = n3->link;
+	/*Linking the previous node with the current node*/
+	n3->link = temp3;
+}
+
+/**
+Description: This function deletes a node from a linked list.
+Param: variable of type Node
+Return: NA
+*/
+void deleteNodeBetween(struct Node* n4)
+{
+	/*Declareing variables*/
+	int deleteNum = 0;
+	printf("\nInput value of node to be deleted: ");
+	scanf("%d",&deleteNum);
+	/*Logic to find the node, to delete the next node after that*/
+	while(n4->link->info != deleteNum)
+	{
+		n4 = n4->link;
+	}
+	/*Logic to link the current node with next to next node bypassing the node to be deleted*/
+	n4->link = n4->link->link;
+}
 
 int main() {
 
@@ -108,9 +210,10 @@ int main() {
 	/*Calling methods to create node, traverse list and search element*/
 	createList(numOfNodes);
 	traverseList(start);
+	insertAtBeginning();
+
 
 	printf("\nNumber to be searched: ");
 	scanf("%d",&searchNum);
 	searchNumMethod(searchNum,start);
-
 }
