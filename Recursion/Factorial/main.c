b369a802dfada42d8868ffb11bfe94939143f00f
Ishan_kansara <ishan.stealth@gmail.com>
2020-04-11 19:02:06 -0400

    Code to calculate factorial usinf recursion

---
+++ Recursion/Factorial/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Declaring global variable
+int result = 0;
+
+/**
+Description: This function finds factorial of a number using recursive method.
+Param: integer
+return: NA
+*/
+int findFactorial(int num)
+{
+    //Logic for base case
+    if(num <= 1)
+    {
+        result = 1;
+        return;
+    }
+    //Logic to calculate factorial recursively.
+    else
+    {
+        result = num * (findFactorial(num-1));
+        printf("\nNum: %d",num);
+        printf("\nResult: %d",result);
+    }
+    return result;
+}
+
+
+int main()
+{
+    printf("Answer: %d",findFactorial(10));
+    return 0;
+}
